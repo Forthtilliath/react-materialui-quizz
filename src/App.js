1eab76280b7f2eea3b075be5c02035caacf36db2
Forth <33458877+Forthtilliath@users.noreply.github.com>
2021-07-05 01:15:39 +0200

    mise en place interface + router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './Pages/Home/Home';
+import Result from './Pages/Result/Result';
+import Quiz from './Pages/Quiz/Quiz';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="app" style={{ backgroundImage: 'url(/assets/images/ques1.png' }}>
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/quiz" exact component={Quiz} />
+                    <Route path="/result" exact component={Result} />
+                    <Redirect to="/" />
+                </Switch>
+            </div>
+            <Footer />
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div>
+            Made with ♥ by&nbsp;
+            <a href="https://github.com/Forthtilliath/" target="_blank" rel="noopener noreferrer">
+                Forth
+            </a>
+        </div>
+    );
+};
+
+export default Footer;
